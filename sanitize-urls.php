f6be8b836d378d0d4a75d4f310eb57ed94858ac6
Tim Mahoney <tim@timothymahoney.com>
2012-06-25 12:30:58 -0700

    Updated sanitize-urls.php

--- sanitize-urls.php
+++ sanitize-urls.php
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
-
+ 
 /**
  * Use this function to make URLs more standard for insertion into your database. 
  * That way, when you search, you can ensure standardization.
